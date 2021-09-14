9acc4cf37089ab374d600401aad6770536bb6418
mohantawar2830 <mrtawar95@gmail.com>
2021-09-14 15:45:32 +0530

    Initial commit

---
+++ src/test/java/com/qa/testcases/LoginTest.java
@@ -0,0 +1,36 @@
+package com.qa.testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+	
+	 static WebDriver driver;
+	
+	@BeforeSuite
+	public void setup()
+	{
+		WebDriverManager.chromedriver().setup();
+		 driver = new ChromeDriver();
+	}
+	@Test
+	public void doLogin()
+	{
+		driver.get("https://classic.freecrm.com/index.html");
+		driver.findElement(By.xpath("//input[@name='username']")).sendKeys("mohantawar");
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys("mohan@2830");
+		driver.findElement(By.xpath("//input[@type='submit']")).click();
+	}
+	@AfterSuite
+	public void teardown()
+	{
+		driver.quit();
+	}
+
+}
